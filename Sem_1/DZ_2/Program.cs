353446c2ecdf11129882b446bdb500037ebc2fab
Dmitriy <dmitrij-balakovo@rambler.ru>
2022-11-26 02:57:17 +0300

    Решение DZ_2 Задача 4

--- Sem_1/DZ_2/Program.cs
+++ Sem_1/DZ_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу,
+// которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int l = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = l;
+if (m > max) max = m;
+if (n > max) max = n;
+Console.Write (max);
