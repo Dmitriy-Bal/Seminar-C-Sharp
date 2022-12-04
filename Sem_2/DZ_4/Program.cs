6f33a8b877e44444516c08965e2d5352851cdd21
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-04 16:38:47 +0300

    Добавил решение задачи №1138

--- Sem_2/DZ_4/Program.cs
+++ Sem_2/DZ_4/Program.cs
@@ -23,10 +23,18 @@
 */
 
 Console.Clear();
-
+Console.WriteLine("Введите числа: ");
 int n = Convert.ToInt32(Console.ReadLine());
+int maxFirst = n, maxSecond = -1;
 while (n != 0)
 {
     n = Convert.ToInt32(Console.ReadLine());
+    if (n > maxFirst)
+    {
+        maxSecond = maxFirst;
+        maxFirst = n;
+    }
+    else if(n > maxSecond)
+        maxSecond = n;
 }
-Console.Write("end");
+Console.Write(maxSecond);
