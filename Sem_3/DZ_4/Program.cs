fa104fc12fede4e4334e59e828271e7c7c19c608
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-06 00:44:35 +0300

    Добавил решение задачи № 496 в папку DZ_4

--- Sem_3/DZ_4/Program.cs
+++ Sem_3/DZ_4/Program.cs
@@ -36,3 +36,20 @@ OUTPUT
 6
 */
 
+Console.Clear();
+Console.Write("Введите количество кустов: ");
+int n = Convert.ToInt32(Console.ReadLine()), resultSum = 0;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < array.Length - 1; i++)
+{
+    if (resultSum < array[i - 1] + array[i] + array[i + 1])
+        resultSum = array[i - 1] + array[i] + array[i + 1];
+}
+if (resultSum < array[n - 1] + array[n - 2] + array[0])
+    resultSum = array[n - 1] + array[n - 2] + array[0];
+if (resultSum < array[0] + array[1] + array[n - 1])
+    resultSum = array[0] + array[1] + array[n - 1];
+Console.WriteLine(resultSum);
