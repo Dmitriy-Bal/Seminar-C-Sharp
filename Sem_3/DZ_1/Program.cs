916cea4436fb12da28ba2e2a001cd546baf0d88c
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-10 16:05:04 +0300

    Добавил второй вариант решения Задачи №19

--- Sem_3/DZ_1/Program.cs
+++ Sem_3/DZ_1/Program.cs
@@ -6,15 +6,34 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.Clear();
-Console.WriteLine("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int n1 = n / 10000;
-int n2 = n / 1000 % 10;
-int n4 = n / 10 % 10;
-int n5 = n % 10;
+// Первый вариант решения
 
-if (n1 == n5 && n2 == n4)
-    Console.Write("Это палиндромом");
-else
-    Console.Write("Это не палиндромом");
+// Console.Clear();
+// Console.WriteLine("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = n / 10000;
+// int n2 = n / 1000 % 10;
+// int n4 = n / 10 % 10;
+// int n5 = n % 10;
+
+// if (n1 == n5 && n2 == n4)
+//     Console.Write("Это палиндромом");
+// else
+//     Console.Write("Это не палиндромом");
+
+
+// Второй вариант решения
+
+Console.Clear();
+Console.Write("Введите число: ");
+string? n = Console.ReadLine();
+int newNumber = 0;
+for (int i = n.Length - 1; i >= 0; i--)
+{
+    newNumber = newNumber + Convert.ToInt32(int.Parse(n[i].ToString()) * Math.Pow(10, i));
+}
+Console.WriteLine(newNumber);
+if (newNumber == int.Parse(n))
+    Console.WriteLine("Это палиндромом");
+else 
+    Console.WriteLine("Это не палиндромом");
