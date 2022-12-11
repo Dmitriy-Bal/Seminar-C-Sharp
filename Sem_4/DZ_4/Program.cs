389dd303d46f99297ec2e3402397384239ece2b7
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-12 00:49:25 +0300

    Добавил задачу № 1220 в папку DZ_4

--- Sem_4/DZ_4/Program.cs
+++ Sem_4/DZ_4/Program.cs
@@ -11,3 +11,30 @@
 а в последней – целое число K. (1 ≤ N ≤ 105, |K| ≤ 105, |Ai| ≤ 100).
 */
 
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int[] result = new int[n];
+
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(1, 11);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
+if (k > 0)
+{
+    for (int i = 0; i < k; i++)
+        result[i] = array[array.Length - k + i];
+    for (int i = 0; i < array.Length - k; i++)
+        result[k + i] = array[i];
+    Console.WriteLine($"[{string.Join(", ", result)}]");
+}
+else
+{
+    k = -k;
+    for (int i = 0; i < k; i++)
+        result[n - k + i] = array[i];
+    for (int i = 0; i < array.Length - k; i++)
+        result[i] = array[k + i];
+    Console.WriteLine($"[{string.Join(", ", result)}]");
+}
