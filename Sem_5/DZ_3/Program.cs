65759ff521348faf6e1342f0e4a2eb31b269555c
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-11 23:44:36 +0300

    Добавил решение Задачи №38 в папку DZ_3

---
+++ Sem_5/DZ_3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int max = array [0];
+    int min = array [0];
+    for (int i =0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+            
+        else if(array[i] < min)
+        {
+            min = array[i];
+        }    
+    }  
+    Console.WriteLine($"Минимальное число: {min}");
+    Console.WriteLine($"Максимальное число: {max}");
+    Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array); 
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
