d7a1e9a8d699c6a1962cf51ee1907f4f9758ab00
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-15 00:02:26 +0300

    Добавил задачу № 41 в папку DZ_1

---
+++ Sem_6/DZ_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223 -> 4
+*/
+
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+int[] m = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < m.Length; i++)
+{
+    if (m[i] > 0)
+        count++;
+}
+ 
+Console.WriteLine($"Количиство чисел больше 0: {count}");
