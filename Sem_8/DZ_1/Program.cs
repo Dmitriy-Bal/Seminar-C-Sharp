102698d230e6564fa94812d9ea596b6ce66d6bb2
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-18 21:19:32 +0300

    Добавил решение Задачи 54 в папку DZ_1

---
+++ Sem_8/DZ_1/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+void OrderArrayLines(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(0, 9);
+    }
+  }
+}
+
+void WriteArray(int[, ] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j] + " "} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+Console.Clear();
+Console.Write("Ведите размер матрицы через пробел: ");
+string[] coord = Console.ReadLine().Split(" ");
+int m = int.Parse(coord[0]);
+int n = int.Parse(coord[1]);
+int[,] matrix = new int[m, n];
+CreateArray(matrix);
+Console.WriteLine("Исходный массив");
+WriteArray(matrix);
+Console.WriteLine($"\nУпорядоченный по убыванию массив: ");
+OrderArrayLines(matrix);
+WriteArray(matrix);
