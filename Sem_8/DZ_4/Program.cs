0b99699ef4db33df63489104aae49f30a14ebe90
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2022-12-20 01:16:26 +0300

    Добавил решение Задачи № 60

--- Sem_8/DZ_4/Program.cs
+++ Sem_8/DZ_4/Program.cs
@@ -8,3 +8,74 @@
 26(1,0,1) 55(1,1,1)
 */
 
+
+void WriteArray (int[,,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"X[{i}]; Y[{j}]; ");
+      for (int k = 0; k < matrix.GetLength(2); k++)
+      {
+        Console.Write( $"Z[{k}]={matrix[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+void CreateArray(int[,,] matrix)
+{
+  int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < matrix.GetLength(0); x++)
+  {
+    for (int y = 0; y < matrix.GetLength(1); y++)
+    {
+      for (int z = 0; z < matrix.GetLength(2); z++)
+      {
+        matrix[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+Console.Clear();
+Console.WriteLine($"\nВведите размер массива X x Y x Z:");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+
+int[,,] matrix = new int[x, y, z];
+CreateArray(matrix);
+WriteArray(matrix);
