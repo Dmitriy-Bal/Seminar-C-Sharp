eec4446f2e4fab48aa85ad77528b9e4ab7b743a3
Dmitriy-Bal <regDmitrij-Balakovo@rambler.ru>
2023-01-07 18:38:38 +0300

    Добавил решение Задачи № 66 в папку DZ_1

---
+++ Sem_9/DZ_1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SumFromMToN(int m, int n, int sum)
+
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    SumFromMToN(m, n, sum);
+}
+
+SumFromMToN(m, n, 0);
